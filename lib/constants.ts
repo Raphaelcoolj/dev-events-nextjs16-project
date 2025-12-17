2841bfb172bef6db2ffebd4374369491c9798982
ekeh <ekehchukwuemeka375@gmail.com>
2025-12-17 16:42:48 +0100

    first commit

---
+++ lib/constants.ts
@@ -0,0 +1,50 @@
+export const events = [
+  {
+    title: "React Summit 2025",
+    image: "/images/event1.png",
+    slug: "react-summit-2025",
+    location: "Amsterdam, Netherlands",
+    date: "June 12-14, 2025",
+    time: "9:00 AM - 6:00 PM"
+  },
+  {
+    title: "JSConf EU 2025",
+    image: "/images/event2.png",
+    slug: "jsconf-eu-2025",
+    location: "Berlin, Germany",
+    date: "May 20-22, 2025",
+    time: "10:00 AM - 7:00 PM"
+  },
+  {
+    title: "Google I/O 2025",
+    image: "/images/event3.png",
+    slug: "google-io-2025",
+    location: "Mountain View, CA, USA",
+    date: "May 14-16, 2025",
+    time: "8:00 AM - 5:00 PM"
+  },
+  {
+    title: "Hack the North 2025",
+    image: "/images/event4.png",
+    slug: "hack-the-north-2025",
+    location: "Waterloo, Canada",
+    date: "January 10-12, 2025",
+    time: "All Day"
+  },
+  {
+    title: "DevFest 2025",
+    image: "/images/event5.png",
+    slug: "devfest-2025",
+    location: "Various Locations",
+    date: "Throughout 2025",
+    time: "Varies"
+  },
+  {
+    title: "PyCon US 2025",
+    image: "/images/event6.png",
+    slug: "pycon-us-2025",
+    location: "Pittsburgh, PA, USA",
+    date: "May 14-22, 2025",
+    time: "9:00 AM - 6:00 PM"
+  }
+];
